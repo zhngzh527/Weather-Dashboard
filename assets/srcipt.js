05fd0e7d412863623009a6b51daa572ceb364991
ZHONGCHENG ZHAO <zhongchengzhao@ZHONGCHENGs-MBP.lan>
2022-10-25 15:57:08 -0500

    still working on forecast

--- assets/srcipt.js
+++ assets/srcipt.js
@@ -1,4 +1,121 @@
-var apiKey = "846c559df3817a2d5728e0e0cabce4f0";
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
+const APIKey = "d1f9beed9f54e1ceb0195af74b70fd9e";
+var today = moment().format('L');
 var searchHistoryList = [];
-var today = moment().format('L')
+
+// function for current condition
+function currentCondition(city) {
+
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=d1f9beed9f54e1ceb0195af74b70fd9e`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(cityWeatherResponse) {
+        console.log(cityWeatherResponse);
+        
+        $("#weatherContent").css("display", "block");
+        $("#cityDetail").empty();
+        
+        var iconCode = cityWeatherResponse.weather[0].icon;
+        var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+        var currentCity = $(`
+            <h2 id="currentCity">
+                ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+            </h2>
+            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+            <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
+            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+        `);
+
+        $("#cityDetail").append(currentCity);
+
+    });
+}
+
+// function for future condition
+function futureCondition(lat, lon) {
+
+    // THEN I am presented with a 5-day forecast
+    var futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=d1f9beed9f54e1ceb0195af74b70fd9e`;
+
+    $.ajax({
+        url: futureURL,
+        method: "GET"
+    }).then(function(futureResponse) {
+        console.log(futureResponse);
+        $("#fiveDay").empty();
+        
+        for (let i = 1; i < 6; i++) {
+            var cityInfo = {
+                date: futureResponse.daily[i].dt,
+                icon: futureResponse.daily[i].weather[0].icon,
+                temp: futureResponse.daily[i].temp.day,
+                humidity: futureResponse.daily[i].humidity
+            };
+
+            var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+            var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" />`;
+
+            var futureCard = $(`
+                <div class="pl-3">
+                    <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
+                        <div class="card-body">
+                            <h5>${currDate}</h5>
+                            <p>${iconURL}</p>
+                            <p>Temp: ${cityInfo.temp} °F</p>
+                            <p>Humidity: ${cityInfo.humidity}\%</p>
+                        </div>
+                    </div>
+                <div>
+            `);
+
+            $("#fiveDay").append(futureCard);
+        }
+    }); 
+}
+
+// add on click event listener 
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+
+    var city = $("#enterCity").val().trim();
+    currentCondition(city);
+    if (!searchHistoryList.includes(city)) {
+        searchHistoryList.push(city);
+        var searchedCity = $(`
+            <li class="list-group-item">${city}</li>
+            `);
+        $("#searchHistory").append(searchedCity);
+    };
+    
+    localStorage.setItem("city", JSON.stringify(searchHistoryList));
+    console.log(searchHistoryList);
+});
+
+$(document).on("click", ".list-group-item", function() {
+    var listCity = $(this).text();
+    currentCondition(listCity);
+});
+
+$(document).ready(function() {
+    var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+
+    if (searchHistoryArr !== null) {
+        var lastSearchedIndex = searchHistoryArr.length - 1;
+        var lastSearchedCity = searchHistoryArr[lastSearchedIndex];
+        currentCondition(lastSearchedCity);
+        console.log(`Last searched city: ${lastSearchedCity}`);
+    }
+});
+
+//This is a test to make sure my APIKey working
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=d1f9beed9f54e1ceb0195af74b70fd9e', {
+//   method: 'GET', 
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
